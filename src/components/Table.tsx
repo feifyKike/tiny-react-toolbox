af9e128b18e7a5377d12ed740b4af7922408ff6b
Maxim Shelepov <mashelep@ncsu.edu>
2025-04-11 13:28:40 -0400

    Standard table component improvements.

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,17 +1,21 @@
-'use client';
-
 import { TableRow } from "../lib/components/table";
 
 interface TableProps {
   headers: string[];
   rows: TableRow[];
+  displayColumns?: string[] | null;
   headersStyling?: string;
   rowsStyling?: string;
   indexCols?: boolean
 }
 
-const Table = ({headers, rows, headersStyling, rowsStyling, indexCols = false}: TableProps) => {
+const Table = ({headers, rows, displayColumns, headersStyling, rowsStyling, indexCols = false}: TableProps) => {
   const displayHeaders = indexCols ? [' ', ...headers] : headers;
+  const displayColumnsIndex = displayColumns ? [' ', ...displayColumns].map((col) => {
+    const index = displayHeaders.indexOf(col)
+    if (index !== -1) return index
+    return null
+  }).filter((col) => col !== null) : null
 
   return (
     <div className="overflow-x-auto">
@@ -19,7 +23,7 @@ const Table = ({headers, rows, headersStyling, rowsStyling, indexCols = false}: 
         <thead>
           <tr className={headersStyling && headersStyling.length > 0 ? headersStyling : 'bg-gray-200'}>
             {
-              displayHeaders.map((header, idx) => <th key={idx} className="px-4 py-2 border-b text-left">{header}</th>)
+              displayHeaders.map((header, idx) => !displayColumns || (displayColumns && displayColumnsIndex?.includes(idx)) ? <th key={idx} className="px-4 py-2 border-b text-left">{header}</th> : '')
             }
           </tr>
         </thead>
@@ -32,7 +36,7 @@ const Table = ({headers, rows, headersStyling, rowsStyling, indexCols = false}: 
               return (
                 <tr key={idx} className={rowsStyling && rowsStyling.length > 0 ? rowsStyling : 'bg-white'}>
                   {
-                    displayCols.map((col, idx) => <td key={idx} className="px-4 py-2 border-b">{col}</td>)
+                    displayCols.map((col, idx) => !displayColumns || (displayColumns && displayColumnsIndex?.includes(idx)) ? <td key={idx} className="px-4 py-2 border-b">{col}</td> : '')
                   }
                 </tr>
               )
